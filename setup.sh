08d08f538edfb76fc42db275f07979c1a36e83b9
hustzxd <2227820226@qq.com>
2021-09-30 16:58:22 +0800

    small fix :heavy_plus_sign:

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 export PATH="/home/${USER}/anaconda3/bin:$PATH"
 export CURRENT_DIR=`pwd`
 export PYTHONPATH=$PYTHONPATH:${CURRENT_DIR}
-export PYTHONPATH=$PYTHONPATH:${CURRENT_DIR}/3rdparty/SimilarSimulator
 
 protoc -I=${CURRENT_DIR}/proto --python_out=${CURRENT_DIR}/proto ${CURRENT_DIR}/proto/efficient_pytorch.proto
 python proto/gene_hyperparam_template.py
